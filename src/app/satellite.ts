c6ccf079131a23d394f0af36b7f9a521b477075c
j-ritenour <61949197+j-ritenour@users.noreply.github.com>
2020-04-09 11:26:42 -0500

    Step 4 Complete

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -6,9 +6,10 @@ operational: boolean;
 launchDate: string;
 constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
     this.name = name;
-    this.orbitType = orbitType;
     this.type = type;
-    this.operational = operational;
     this.launchDate = launchDate;
+    this.orbitType = orbitType;
+    this.operational = operational;
+    
 }
 }
